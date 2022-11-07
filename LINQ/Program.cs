3b80c3bb7ac17ef9253b0cf4f326f1670ced8295
Yvette Lerma <lerma.yvette@hotmail.com>
2022-11-07 14:11:18 -0700

    you are doing great

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameList = new List<string>() { "Minecraft", "CoD", "Halo", "Little Big Planet", "FortNite" };
+
+
+            //var gamesListOrdered = videoGameList.OrderBy(name => name.Length).ToList();
+
+            //foreach (var game in gamesListOrdered)
+            //{
+            //    Console.WriteLine(game);
+            //}
+
+            //gamesListOrdered.ForEach(Console.WriteLine);
+
+            videoGameList.OrderBy(name => name.Length).ToList().ForEach(Console.WriteLine);
         }
     }
 }
